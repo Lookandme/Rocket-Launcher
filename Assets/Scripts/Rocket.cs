baf081e01d53f3562d8afb180af52f1879f4b988
Lookandme <kgg9386@gmail.com>
2024-10-16 13:01:22 +0900

    fix : Rocket.cs

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,7 +4,7 @@ public class Rocket : MonoBehaviour
 {
     private Rigidbody2D _rb2d;
     private float fuel = 100f;
-    GameObject rocket;
+    
     
     
     
@@ -17,14 +17,20 @@ public class Rocket : MonoBehaviour
         _rb2d = GetComponent<Rigidbody2D>();
     }
     
+
     public void Shoot()
     {
         // TODO : fuel이 넉넉하면 윗 방향으로 SPEED만큼의 힘으로 점프, 모자라면 무시
-        if(fuel == 0f)return;
-        else if(fuel > 10f)
+        
+        if (fuel == 0f) return;
+        else if (fuel >= FUELPERSHOOT)
         {
-           _rb2d.AddForce(Vector2.up *  SPEED);
+            _rb2d.AddForce(transform.up * SPEED, ForceMode2D.Impulse); // 힘의 방향, 힘의 크기, 힘의 형태를 매개변수로 가지고 있다.
             fuel -= FUELPERSHOOT;
+            
         }
+        
+        
+        
     }
 }
